2ac8280c933b4dfed18db666ad85ed7a11f7482d
ybenlafk <yben0600@gmail.com>
2025-01-02 15:57:47 +0100

    p2

--- p2/scripts/install.sh
+++ p2/scripts/install.sh
@@ -9,6 +9,7 @@ apt-get install -y curl net-tools
 
 # Install K3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -s - --write-kubeconfig-mode 644
+
 # Wait for K3s to be ready
 while ! kubectl get node | grep -q "Ready"; do
     sleep 2
