63ee87c0a6e1451025272e5b40f040641ec3e1f8
ybenlafk <yben0600@gmail.com>
2025-01-03 16:36:04 +0100

    p2

--- p1/scripts/setup-common.sh
+++ p1/scripts/setup-common.sh
@@ -3,6 +3,6 @@
 echo "Setting up common environment..."
 
 sudo apt-get update -y
-sudo apt-get install -y curl apt-transport-https
+sudo apt-get install -y curl apt-transport-https net-tools
 
 echo "Common environment setup complete."
