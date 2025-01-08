ae2d8c3eee756ca502feea03ff1923ad129625e1
ybenlafk <yben0600@gmail.com>
2025-01-08 13:35:44 +0100

    p1

--- p1/confs/controller-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Setting up K3s controller on ybenlafkS..."
-
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -s - --write-kubeconfig-mode 644
-
-echo "K3s controller setup complete."
