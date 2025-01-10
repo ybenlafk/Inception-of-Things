9545aa7d87165fca8e7f615c06e3de8b6656381c
samini <soufianeamini2002@gmail.com>
2025-01-10 11:21:39 +0100

    setup argocd

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,4 +1,22 @@
+# TODO: Dependencies 
+# - curl
+# - kubectl
+# - docker
+# - argocd cli
 # Cluster configurations
+
+k3d cluster create -p "8888:8888@loadbalancer" part3
+
+# Dev
+
 kubectl create -f ../config/dev-namespace.yaml
 kubectl apply -f ../config/wil-app.yaml
-kubectl apply -f ../config/ingress.yaml
+kubectl apply -f ../config/wil-ingress.yaml
+
+# Argocd
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 5
+kubectl wait pod --all --for=condition=Ready --namespace=argocd --timeout=-1s
+kubectl port-forward svc/argocd-server -n argocd 8080:443
