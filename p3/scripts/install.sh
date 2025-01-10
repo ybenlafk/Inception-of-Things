3c9ed9be527b35fab1ce987b4fc4f73900862bfb
samini <soufianeamini2002@gmail.com>
2025-01-10 12:29:48 +0100

    add argocd commands for login, app creation, will need to export config

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -20,11 +20,7 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 sleep 5
 kubectl wait pod --all --for=condition=Ready --namespace=argocd --timeout=-1s
 kubectl port-forward svc/argocd-server -n argocd 8080:443& disown
-
-# TODO: remove testing exit
-exit
-
 argocd login localhost:8080 --username admin --password $(argocd admin initial-password -n argocd | cut -d\  -f1) --insecure
-argocd cluster add k3d-part3
-
-
+argocd cluster add k3d-part3 --insecure -y --in-cluster # FIXME: This might not be necessary
+kubectl config set-context --current --namespace=argocd
+argocd app create wil-app --repo https://github.com/soufianeamini/wil-app.git --path wil-app --dest-server https://kubernetes.default.svc --dest-namespace dev
