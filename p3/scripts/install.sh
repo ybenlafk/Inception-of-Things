fcb73c6d0f223bb9340474a44dbdb628b107e5db
samini <soufianeamini2002@gmail.com>
2025-01-11 00:35:02 +0100

    in progress

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -9,9 +9,9 @@ k3d cluster create -p "8888:8888@loadbalancer" part3
 
 # Dev
 
-kubectl create -f ../config/dev-namespace.yaml
-kubectl apply -f ../config/wil-app.yaml
-kubectl apply -f ../config/wil-ingress.yaml
+# kubectl create -f ../config/dev-namespace.yaml
+# kubectl apply -f ../config/wil-app.yaml
+# kubectl apply -f ../config/wil-ingress.yaml
 
 # Argocd
 
@@ -22,7 +22,8 @@ echo "Waiting for argocd pods to be ready.."
 kubectl wait pod --all --for=condition=Ready --namespace=argocd --timeout=-1s
 kubectl port-forward svc/argocd-server -n argocd 8080:443& disown
 argocd login localhost:8080 --username admin --password $(argocd admin initial-password -n argocd | cut -d\  -f1) --insecure
-argocd cluster add k3d-part3 --insecure -y --in-cluster # FIXME: This might not be necessary
+# argocd cluster add k3d-part3 --insecure -y --in-cluster # FIXME: This might not be necessary
 kubectl config set-context --current --namespace=argocd
+# TODO: replace the path below with the p3/config/
 argocd app create wil-app --repo https://github.com/soufianeamini/wil-app.git --path . --dest-server https://kubernetes.default.svc --dest-namespace dev
 argocd app set wil-app --sync-policy automated
