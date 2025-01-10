b43f36a02a636f2fd44d78b3c07d097f1b05f51e
samini <soufianeamini2002@gmail.com>
2025-01-10 11:50:31 +0100

    argocd login

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -19,4 +19,12 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 sleep 5
 kubectl wait pod --all --for=condition=Ready --namespace=argocd --timeout=-1s
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+kubectl port-forward svc/argocd-server -n argocd 8080:443& disown
+
+# TODO: remove testing exit
+exit
+
+argocd login localhost:8080 --username admin --password $(argocd admin initial-password -n argocd | cut -d\  -f1) --insecure
+argocd cluster add k3d-part3
+
+
