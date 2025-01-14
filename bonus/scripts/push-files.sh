b514699648c56d4d15fb4db20841d453a0c61122
admin <gitlabadmin@example.com>
2025-01-14 14:47:45 +0100

    push files to repo

--- bonus/scripts/push-files.sh
+++ bonus/scripts/push-files.sh
@@ -1,4 +1,4 @@
-git clone http://localhost:bonus_pass/root/bonus_project.git
+git clone http://root:bonus_pass@localhost/root/bonus_project.git
 cp ../config/*.yaml bonus_project
 cd bonus_project
 git config user.name "admin"
@@ -6,3 +6,4 @@ git config user.email "gitlabadmin@example.com"
 git add .
 git commit -m "first commit"
 git push -u origin main
+rm -rf bonus_project
