8e483eee2536b1c6a98a75601300fe5d785f10ca
admin <gitlabadmin@example.com>
2025-01-14 15:05:21 +0100

    connect to gitlab service, need to add gitlab to k8s cluster

---
+++ bonus/scripts/get-argocd-pass.sh
@@ -0,0 +1,1 @@
+argocd admin initial-password -n argocd
--- bonus/scripts/run.sh
+++ bonus/scripts/run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 k3d cluster create -p "8888:8888@loadbalancer" bonus
 
 # Argocd
@@ -9,7 +10,7 @@ echo "Waiting for argocd pods to be ready.."
 kubectl wait pod --all --for=condition=Ready --namespace=argocd --timeout=-1s
 kubectl port-forward svc/argocd-server -n argocd 9091:443& disown
 argocd login localhost:9091 --username admin --password $(argocd admin initial-password -n argocd | cut -d\  -f1) --insecure
-argocd cluster add k3d-part3 --insecure -y --in-cluster
+argocd cluster add k3d-bonus --insecure -y --in-cluster
 kubectl config set-context --current --namespace=argocd
-argocd app create wil-app --repo http://localhost/root/wil-app.git --path . --dest-server https://kubernetes.default.svc --dest-namespace dev
+argocd app create wil-app --repo http://gitlab-service/root/bonus_project.git --path . --dest-server https://kubernetes.default.svc --dest-namespace dev
 argocd app set wil-app --sync-policy automated
