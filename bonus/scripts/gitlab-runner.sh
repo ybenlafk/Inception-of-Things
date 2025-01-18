dfbc8e9e74290b59d6f1c0edd7e691a830b59472
samini <soufianeamini2002@gmail.com>
2025-01-18 21:45:39 +0100

    update volumes

--- bonus/scripts/gitlab-runner.sh
+++ bonus/scripts/gitlab-runner.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-k3d cluster create -p "8888:8888@loadbalancer" bonus
+mkdir -p /tmp/gitlab-config /tmp/gitlab-opt /tmp/gitlab-log /tmp/gitlab-bonus
+k3d cluster create -v /tmp/gitlab-config:/tmp/gitlab-config -v /tmp/gitlab-log:/tmp/gitlab-log -v /tmp/gitlab-opt:/tmp/gitlab-opt -v /tmp/gitlab-bonus:/tmp/gitlab-bonus -p "8888:8888@loadbalancer" bonus
 
-mkdir -p /tmp/gitlab-scripts
-cp ../config/wil-app/*.yaml /tmp/gitlab-scripts
-cp ./gitlab-configure.sh /tmp/gitlab-scripts
+cp ../config/wil-app/*.yaml /tmp/gitlab-bonus
+cp ./gitlab-configure.sh /tmp/gitlab-bonus
 
 kubectl apply -f ../config/gitlab-app.yaml
