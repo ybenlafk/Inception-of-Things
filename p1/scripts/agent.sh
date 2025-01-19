45c86fc03fdd6126c60564b28454c7f170618ba5
admin <gitlabadmin@example.com>
2025-01-19 12:06:37 +0100

    update

--- p1/scripts/agent.sh
+++ p1/scripts/agent.sh
@@ -1,6 +1,3 @@
-echo "hello i'm agent"
-
-TOKEN=$(cat /vagrant/.node-token)
-echo "============> " $TOKEN
+TOKEN="$(cat /vagrant/.node-token)"
 
 curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$TOKEN sh -s - --node-ip 192.168.56.111
--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,5 +1,3 @@
-echo "hello i'm server"
-
 curl -sfL https://get.k3s.io | sh -s - --node-ip 192.168.56.110 --write-kubeconfig-mode 644
 
-sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/ && sudo mv /vagrant/node-token /vagrant/.node-token
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/.node-token
