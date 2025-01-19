618f22b30f20a0daba8cd3f7f146192f031da1ea
samini <soufianeamini2002@gmail.com>
2025-01-19 10:11:41 +0100

    some fixes

--- bonus/scripts/gitlab-configure.sh
+++ bonus/scripts/gitlab-configure.sh
@@ -1,16 +1,17 @@
 create_gitlab_project() {
-	gitlab-rails runner "project = Project.create( name: 'bonus_project', path: 'bonus_project', creator_id: user.id, visibility_level: 20, namespace_id: 1, organization_id: 1); project.save!"
+	gitlab-rails runner "project = Project.create( name: 'bonus_project', path: 'bonus_project', creator_id: user.id, visibility_level: 20, namespace_id: 1, organization_id: 1); project.save!; project.create_repository()"
 }
 
 push_project() {
 	git clone http://root:password123456@localhost/root/bonus_project.git
 	cp /mnt/bonus/wil-app/*.yaml bonus_project
-	cd bonus_project
+	cd bonus_project || echo "Error: bonus_project wasn't cloned successfully, exiting..." && exit 1
 	git config user.name "admin"
 	git config user.email "gitlabadmin@example.com"
 	git add .
 	git commit -m "first commit"
 	git push -u origin main
+	cd ..
 	rm -rf bonus_project
 }
 
--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -22,7 +22,7 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 newgrp docker
 
 # k3d
