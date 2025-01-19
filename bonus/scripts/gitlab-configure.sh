9818a8637070715f6aca866685dcb7ff0d97925b
admin <gitlabadmin@example.com>
2025-01-19 11:32:52 +0100

    bonus done

--- bonus/scripts/gitlab-configure.sh
+++ bonus/scripts/gitlab-configure.sh
@@ -1,11 +1,11 @@
 create_gitlab_project() {
-	gitlab-rails runner "project = Project.create( name: 'bonus_project', path: 'bonus_project', creator_id: user.id, visibility_level: 20, namespace_id: 1, organization_id: 1); project.save!; project.create_repository()"
+	gitlab-rails runner "project = Project.create( name: 'bonus_project', path: 'bonus_project', creator_id: 1, visibility_level: 20, namespace_id: 1, organization_id: 1); project.save!; project.create_repository()"
 }
 
 push_project() {
 	git clone http://root:password123456@localhost/root/bonus_project.git
 	cp /mnt/bonus/wil-app/*.yaml bonus_project
-	cd bonus_project || echo "Error: bonus_project wasn't cloned successfully, exiting..." && exit 1
+	cd bonus_project || (echo "Error: bonus_project wasn't cloned successfully, exiting..." && exit 1)
 	git config user.name "admin"
 	git config user.email "gitlabadmin@example.com"
 	git add .
