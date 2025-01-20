b3c037d046b7102676012297c86d6c1b1ce064a2
Your Name <you@example.com>
2025-01-20 13:00:38 +0100

    wil-app

--- bonus/scripts/gitlab-runner.sh
+++ bonus/scripts/gitlab-runner.sh
@@ -2,7 +2,8 @@
 mkdir -p /tmp/gitlab-config /tmp/gitlab-opt /tmp/gitlab-log /tmp/gitlab-bonus
 k3d cluster create -v /tmp/gitlab-config:/tmp/gitlab-config -v /tmp/gitlab-log:/tmp/gitlab-log -v /tmp/gitlab-opt:/tmp/gitlab-opt -v /tmp/gitlab-bonus:/tmp/gitlab-bonus -p "8888:8888@loadbalancer" bonus
 
-cp ../config/wil-app/*.yaml /tmp/gitlab-bonus
+mkdir -p /tmp/gitlab-bonus/wil-app
+cp ../config/wil-app/*.yaml /tmp/gitlab-bonus/wil-app
 cp ./gitlab-configure.sh /tmp/gitlab-bonus
 
 kubectl apply -f ../config/gitlab-app.yaml
