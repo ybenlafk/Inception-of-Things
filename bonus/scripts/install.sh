efc3d50f197d43936687be2beec37342d3f9e067
Your Name <you@example.com>
2025-01-20 12:31:50 +0100

    newgrp

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -23,7 +23,6 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 sudo groupadd docker
 sudo usermod -aG docker "$USER"
-newgrp docker
 
 # k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
@@ -32,3 +31,5 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
+
+newgrp docker
